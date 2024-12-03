25d80243631f20770577b5f1557cf31a43caf862
Hiệu burando Tô <hieutotoka@gmail.com>
2024-12-03 20:17:50 +0700

    FInal

--- PL/Program.cs
+++ PL/Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<IArtistService, ArtistService>();
-
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7157/api/")
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
